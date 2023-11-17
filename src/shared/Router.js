ef791c9eb64e40ac4ba4f1f9156eddadd3522e1b
cheolminJOO <cheolmin.joo@gmail.com>
2023-11-17 15:02:57 +0900

    outlet

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -5,17 +5,20 @@ import Home from '../pages/Home'
 import DetailedPage from '../pages/DetailedPage'
 import KaKaoLoginTest from '../pages/KaKaoLoginTest'
 import KakaoAuth from '../pages/KakaoAuth'
+import Layout from '../ui/Layout'
 
 export const Router = () => {
   return (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element= {<Login/>} />
-      <Route path="/signup" element= {<SignUp/>} />
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/mypage" element={<KaKaoLoginTest/>}/>
-      <Route path="mylist/:id" element={<DetailedPage/>}/>
-      <Route path="/auth" element={<KakaoAuth/>}/>
+      <Route path="/" element ={<Layout/>}>
+        <Route index element= {<Login/>} />
+        <Route path="/signup" element= {<SignUp/>} />
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/mypage" element={<KaKaoLoginTest/>}/>
+        <Route path="mylist/:id" element={<DetailedPage/>}/>
+        <Route path="/auth" element={<KakaoAuth/>}/>
+      </Route>
     </Routes>
   </BrowserRouter>
   )
--- src/ui/Layout.jsx
+++ src/ui/Layout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { Outlet } from 'react-router-dom'
 
-export default function Layout({children}) {
+export default function Layout() {
   return (
     <div style={{minHeight: '100vh', display: 'flex', flexDirection: 'column'}}>
       <Header/>
         <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          {children}
+          <Outlet/>
         </div>
       <Footer/>
     </div>
